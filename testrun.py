d186404edca097808311bd8b134df3d8ff28d37c
Joseph Peter <josephvpeter7@gmail.com>
2022-11-09 14:20:56 -0500

    updated code for getting the bucket list

--- testrun.py
+++ testrun.py
@@ -1,15 +1,26 @@
 import boto3
-from boto3 import Session #not needed if we have boto3 imported alread 
+# bash this cmd gives iam users list - "aws iam list-users"
+# from boto3 import Session #not needed if we have boto3 imported already 
+# boto_sess = Session(profile_name='useBoto3') # selects useBoto3 aws cli profile 
+aws_resource_s3 = boto3.client("s3") # created a s3 object
+#response = s3.describe_instance() # AttributeError: 'S3' object has no attribute 'describe_instance' 
+#print(response)
+# list_s3_obj = s3.list_objects(Bucket="s3")["Contents"] # work after cli profile configuration
+# print(len(list_s3_obj))
 
-boto_sess = Session(profile_name='useBoto3') # selects useBoto3 aws cli profile 
-s3 = boto3.client("s3")
-list_s3 = s3.list_objects(Bucket="s3")["Contents"]
-print(len(list_s3))
-print(s3)
-try:
-    del s3
+response = aws_resource_s3.list_buckets()
+buckets = response["Buckets"]
 
-except:
-    print("failed")
+for bucket in buckets:
+    print("S3 bucket name : ",bucket["Name"])
 
-print("Looks like S3 is Deleted !")
+
+
+# print("Created S3 object : ",s3)
+# try:
+#     del s3
+
+# except:
+#     print("failed")
+
+# print("Looks like S3 is Deleted !")
