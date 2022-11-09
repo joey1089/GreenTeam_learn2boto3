c88d32c68732b28a42cd3edfd4cfd53526b87234
Joseph Peter <josephvpeter7@gmail.com>
2022-11-09 16:39:24 -0500

    updated creates3.py

--- s3/creates3.py
+++ s3/creates3.py
@@ -4,7 +4,7 @@ resource_s3 = boto3.resource("s3")
 get_userinput = str(input("Enter the unique bucket name : "))
 get_bucketname = resource_s3.Bucket(get_userinput)
 # Creates a bucket with ACL as public read
-# response = get_bucketname.create_bucket() doesn't accept arugment without bucket 
+# response = get_bucketname.create_bucket() doesn't accept arugments without Bucket passed
 response = get_bucketname.create(  
     ACL = 'private',
     CreateBucketConfiguration={
