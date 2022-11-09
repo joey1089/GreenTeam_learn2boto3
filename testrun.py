217cbb2eb8da8cfc830bbe22f6ee412e30fdfa41
Joseph Peter <josephvpeter7@gmail.com>
2022-11-09 13:10:35 -0500

    updated code for selecting cli profile

--- testrun.py
+++ testrun.py
@@ -1,6 +1,10 @@
 import boto3
+from boto3 import Session #not needed if we have boto3 imported alread 
 
+boto_sess = Session(profile_name='useBoto3') # selects useBoto3 aws cli profile 
 s3 = boto3.client("s3")
+list_s3 = s3.list_objects(Bucket="s3")["Contents"]
+print(len(list_s3))
 print(s3)
 try:
     del s3
