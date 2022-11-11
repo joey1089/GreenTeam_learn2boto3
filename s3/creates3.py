44d3f91c8b2b04e2d8b6708a377f167c2c82ed2c
Joseph Peter <josephvpeter7@gmail.com>
2022-11-10 22:44:32 -0500

    updated code on creates3.py

--- s3/creates3.py
+++ s3/creates3.py
@@ -1,5 +1,14 @@
 import boto3
 
+# gets the list of buckets from S3
+resource_s3_list = boto3.client("s3")
+get_response = resource_s3_list.list_buckets()
+buckets = get_response["Buckets"]
+
+for bucket in buckets:
+    print("S3 bucket name : ",bucket["Name"])
+
+# Create a new bucket
 resource_s3 = boto3.resource("s3")
 get_userinput = str(input("Enter the unique bucket name : "))
 get_bucketname = resource_s3.Bucket(get_userinput)
@@ -8,16 +17,10 @@ get_bucketname = resource_s3.Bucket(get_userinput)
 response = get_bucketname.create(  
     ACL = 'private',
     CreateBucketConfiguration={
-        'LocationConstraint': 'us-east-2' #check why only us-east-2 is accepted
+        'LocationConstraint': 'us-east-1' #check why only us-east-2 is accepted
     },
 )
 print("s3 response :",response)
-# gets the list of buckets from S3
-resource_s3_list = boto3.client("s3")
-get_response = resource_s3_list.list_buckets()
-buckets = get_response["Buckets"]
 
-for bucket in buckets:
-    print("S3 bucket name : ",bucket["Name"])
 
 
