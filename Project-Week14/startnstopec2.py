0aab6888a9d2f0c65c2077d81a909f135b01cf8b
Joseph Peter <josephvpeter7@gmail.com>
2022-11-19 10:59:39 -0500

    updated code to make it more presentable

--- Project-Week14/startnstopec2.py
+++ Project-Week14/startnstopec2.py
@@ -3,10 +3,16 @@
 #<----- Work pending in intergration to call each fn to perform operations whenever needed ------>
 import boto3
 
-getObj_ec2 = boto3.client('ec2') #Creating object to create the ec2 instances
+# Resources Objects are created here
+getObj_ec2 = boto3.client('ec2') # Resource object to create the ec2 instances
+stop_ec2 = boto3.resource('ec2', region_name='us-east-1') # Resource Object to stop the ec2 instances
+start_ec2 = boto3.resource('ec2', region_name='us-east-1') # Resource Object to start the ec2 instances
+terminate_ec2 = boto3.client('ec2') # Resources Object to terminate the ec2 instances
+
 
 
 def create_ec2(ec2Obj,Min,Max,imageid,instancetype):
+    '''To create ec2 instances'''
     
     get_response = ec2Obj.run_instances(ImageId=imageid,
             InstanceType=instancetype,
@@ -37,8 +43,8 @@ for instance in get_created_ec2:
     print(f"Instance ID : {instance['InstanceId']} , Type : {instance['InstanceType']}")
 
 #Code to start the stopped ec2 instances
-start_ec2 = boto3.resource('ec2', region_name='us-east-1')
 def starting_instances(ec2):
+    '''Start the stopped ec2 instances'''
     starting_list = []
     stopped_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']},{'Name': 'tag:Environment','Values':['Dev']}])
     for instance in stopped_instances:
@@ -50,9 +56,8 @@ get_starting_list = starting_instances(start_ec2)
 
 
 #Code to stop running ec2 instances
-stop_ec2 = boto3.resource('ec2', region_name='us-east-1')
-
 def stopped_environment_instances(ec2):
+    '''Stop the ec2 instances'''
     stopped_list = []
     running_environment_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running']},{'Name': 'tag:Environment','Values':['Dev']}])
     for instance in running_environment_instances:
@@ -64,9 +69,8 @@ def stopped_environment_instances(ec2):
 get_stopped_list = stopped_environment_instances(stop_ec2)
 print("Get the response : ",get_stopped_list)
 
-# Terminate all ec2 instances
-terminate_ec2 = boto3.client('ec2')
 
+# Terminate all ec2 instances
 def termin_ec2(ec2Obj):
     ''' This fn needs ec2 obj as argument and it terminates all instance found in the acccount and returns response list'''
     terminate_ec2 = []
@@ -80,4 +84,4 @@ def termin_ec2(ec2Obj):
 # print("Terminated EC2 Instance list : ",list_termin_ec2)
 #call the fn
 terminated_ec2 =termin_ec2(terminate_ec2)
-print(terminated_ec2)
+print("Get the response : ",terminated_ec2)
