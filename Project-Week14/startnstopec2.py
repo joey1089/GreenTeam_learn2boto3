492198b2e71279b6913b8cbe6783ee16f4e9b425
Joseph Peter <josephvpeter7@gmail.com>
2022-11-19 09:52:46 -0500

    updated code with start ec2 instances

--- Project-Week14/startnstopec2.py
+++ Project-Week14/startnstopec2.py
@@ -33,6 +33,20 @@ print("List of Created Instances : ")
 for instance in get_created_ec2:
     print(f"Instance ID : {instance['InstanceId']} , Type : {instance['InstanceType']}")
 
+#Code to start the stopped ec2 instances
+import boto3
+ec2 = boto3.resource('ec2', region_name='us-east-1')
+def starting_instances(ec2):
+    starting_list = []
+    stopped_instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['stopped']},{'Name': 'tag:Environment','Values':['Dev']}])
+    for instance in stopped_instances:
+        id=instance.id        
+        starting_list.append(ec2.instances.filter(InstanceIds=[id]).start())
+    return starting_list
+
+get_starting_list = starting_instances(ec2)
+
+
 #Code to stop running ec2 instances
 import boto3
 
